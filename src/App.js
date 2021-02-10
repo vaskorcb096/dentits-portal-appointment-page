dabee6066eca43770e4e484ffc60e23440f6a34c
vaskorcb096 <vchakroborty096@gmail.com>
2021-02-10 15:46:41 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { createContext, useState } from 'react';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './components/Home/Home/Home';
+import Appointment from './components/Appointment/Appointment/Appointment';
+import Dashboard from './components/Dashboard/Dashboard/Dashboard';
+import Login from './components/Login/Login/Login';
+import AllPatients from './components/AllPatients/AllPatients/AllPatients';
+
+export const UserContext = createContext();
+
+function App() {
+  const [loggedInUser, setLoggedInUser] = useState({});
+  return (
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+      <Router>
+        <Switch>
+          <Route path="/appointment">
+            <Appointment></Appointment>
+          </Route>
+          <Route path="/dashboard/appointment">
+            <Dashboard></Dashboard>
+          </Route>
+          <Route path="/dashboard/allPatients">
+            <AllPatients></AllPatients>
+          </Route>
+          <Route path="/login">
+            <Login></Login>
+          </Route>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+        </Switch>
+      </Router>
+    </UserContext.Provider>
+  );
+}
+
+export default App;
